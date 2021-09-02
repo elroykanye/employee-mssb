7c0a7f20a08d8c9d70c45d2198b847c9b706152a
Arel meilleur <arelkeubou@gmail.com>
2021-09-02 15:55:47 +0100

    changement du fichier workspace et creation de quelque entites

--- src/main/java/axxentis/intenship/laboratoireapi/entities/City.java
+++ src/main/java/axxentis/intenship/laboratoireapi/entities/City.java
@@ -1,22 +1,23 @@
 package axxentis.intenship.laboratoireapi.entities;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Department {
+public class City {
 
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
-    private String description;
 }
--- src/main/java/axxentis/intenship/laboratoireapi/entities/Task.java
+++ src/main/java/axxentis/intenship/laboratoireapi/entities/Task.java
@@ -1,22 +1,25 @@
 package axxentis.intenship.laboratoireapi.entities;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Department {
+public class Task {
 
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
 
-    private String name;
+    private String title;
     private String description;
+
 }
