75dde3da1d7ddde6e5bdc3d9fb2d9ab0078533a6
Shelby <krolshelby21@gmail.com>
2021-09-02 11:42:06 +0100

    Structure de base du projet

---
+++ src/main/java/axxentis/intenship/laboratoireapi/LaboratoireApiApplication.java
@@ -0,0 +1,13 @@
+package axxentis.intenship.laboratoireapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoireApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratoireApiApplication.class, args);
+    }
+
+}
