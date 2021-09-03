a175616b924b15904075fc1d276af26ebf3ba35c
Arel meilleur <arelkeubou@gmail.com>
2021-09-03 16:12:39 +0100

    Implémentation des controllers

--- src/main/java/axxentis/intenship/laboratoireapi/controllers/CoutryController.java
+++ src/main/java/axxentis/intenship/laboratoireapi/controllers/CoutryController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/country")
-public class CoutryCOntroller {
+public class CoutryController {
 
     @Autowired
     CountryService countryService;
