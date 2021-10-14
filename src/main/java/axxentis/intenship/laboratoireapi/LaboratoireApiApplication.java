8ba7d02938da5f98eee94b7b16a7d5fd140e6b2a
elroykanye <elroykanye@gmail.com>
2021-10-14 20:52:30 +0100

    fixed relation bug on employee: modified ./src/main/java/axxentis/intenship/laboratoireapi/LaboratoireApiApplication.java

--- src/main/java/axxentis/intenship/laboratoireapi/LaboratoireApiApplication.java
+++ src/main/java/axxentis/intenship/laboratoireapi/LaboratoireApiApplication.java
@@ -3,13 +3,29 @@ package axxentis.intenship.laboratoireapi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Slf4j
 @SpringBootApplication
+@EnableSwagger2
 public class     LaboratoireApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LaboratoireApiApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
