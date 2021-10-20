ab1ec2cb05e7b7be1de42584351dccb0a91c8087
elroykanye <elroykanye@gmail.com>
2021-10-20 05:13:57 +0100

    traced and resolve bugs at: ./src/main/java/axxentis/intenship/laboratoireapi/mapper/TaskMapper.java

--- src/main/java/axxentis/intenship/laboratoireapi/mapper/TaskMapper.java
+++ src/main/java/axxentis/intenship/laboratoireapi/mapper/TaskMapper.java
@@ -6,26 +6,17 @@ import axxentis.intenship.laboratoireapi.entities.Task;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 
 @Mapper(componentModel = "spring", implementationPackage = "<PACKAGE_NAME>.impl")
 public interface TaskMapper {
 
-    @Mapping(target = "assigneeEmail", expression = "java(mapEmployee(task.getEmployee()))")
-    @Mapping(target = "taskComplete", expression = "java(mapTaskComplete(task.getTaskComplete()))")
     TaskDto mapTaskToDto(Task task);
 
     @InheritInverseConfiguration
-    @Mapping(target = "employee", ignore = true)
+    @Mapping(target = "schedule", ignore = true)
     Task mapDtoToTask(TaskDto taskDto);
-
-    default String mapEmployee(Employee employee) {
-        return employee.getEmail();
-    }
-
-    default Boolean mapTaskComplete(Boolean taskComplete) {
-        return taskComplete != null && taskComplete;
-    }
 
 
 }
