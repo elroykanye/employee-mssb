89751e78448ecc8c2959dcb853a57740c3e4f4e6
elroykanye <elroykanye@gmail.com>
2021-10-20 05:14:01 +0100

    traced and resolve bugs at: ./src/main/java/axxentis/intenship/laboratoireapi/entities/Schedule.java

--- src/main/java/axxentis/intenship/laboratoireapi/entities/Schedule.java
+++ src/main/java/axxentis/intenship/laboratoireapi/entities/Schedule.java
@@ -3,50 +3,59 @@ package axxentis.intenship.laboratoireapi.entities;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "tbl_schedule")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id") // // use to correct Infinite Recursion
-@Table(name = "schedule")
-public class Schedule extends Common {
+public class Schedule {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "SCHEDULE_ID", updatable = false, nullable = false)
     private Long id;
 
     @NotBlank(message = "Schedule Title cannot be empty or Null")
-    @Column(name = "title", updatable = true)
     private String title;
-    @Column(columnDefinition="Weekly tasks", nullable = true)
+
+    @Column(columnDefinition="Weekly tasks", nullable = false)
     private String task;
+
     private String startDate;
+
     private String finishDate;
+
     private Long percentageComplete;
-    private Boolean status;
+
+    private Boolean scheduleStatus;
+
     private String observation;
-    private Long HR_result;
-    private Long supervisor_result;
-    private String HR_observation;
-    private String supervisor_observation;
 
+    private Long hrResult;
 
+    private Long supervisorResult;
+
+    private String hrObservation;
+
+    private String supervisorObservation;
+
+    private Boolean scheduleComplete;
 
     // Relationships
+    // not getting the use of the annotations below
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference()
     @ManyToOne(targetEntity = Employee.class, fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, name = "CONTACT_ID")
-    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-    @JsonIdentityReference(alwaysAsId = false)
     private Employee employee;
 
     @OneToMany(targetEntity = Task.class, mappedBy = "schedule", fetch = FetchType.LAZY)
